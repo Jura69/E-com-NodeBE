e178912c32118c78013368bdd4dab1a8ec3982a1
Truong Tuan Loc <loctruongtuan@gmail.com>
2024-12-11 17:57:01 +0700

    ErrorHandler

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -33,17 +33,6 @@ class AccessService {
 
         if (newShop) {
             // create private, public key
-            /* const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-                modulusLength: 4096,
-                publicKeyEncoding: {
-                    type: 'pkcs1',
-                    format: 'pem',
-                },
-                privateKeyEncoding: {
-                    type: 'pkcs1',
-                    format: 'pem',
-                } 
-            }) */
             const privateKey = crypto.randomBytes(64).toString('hex');
             const publicKey = crypto.randomBytes(64).toString('hex');
 
